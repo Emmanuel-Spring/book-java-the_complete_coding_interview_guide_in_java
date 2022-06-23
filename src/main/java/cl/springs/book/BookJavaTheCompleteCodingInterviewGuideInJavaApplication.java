2c2fcc7a503be3fd09cb07e0a8d9335cf756d816
Emmanuel-Spring <e.nieto@alumnos.santotomas.cl>
2022-06-23 11:58:09 -0400

    primer commit

---
+++ src/main/java/cl/springs/book/BookJavaTheCompleteCodingInterviewGuideInJavaApplication.java
@@ -0,0 +1,13 @@
+package cl.springs.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookJavaTheCompleteCodingInterviewGuideInJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookJavaTheCompleteCodingInterviewGuideInJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/springs/book/BookJavaTheCompleteCodingInterviewGuideInJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.springs.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookJavaTheCompleteCodingInterviewGuideInJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
